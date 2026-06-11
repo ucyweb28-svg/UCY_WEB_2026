abaf11abf812a76c3bc2d31f87dcc07ee3f14fd7
ucyweb28-svg <ucyweb28@gmail.com>
2026-06-11 13:44:18 +0300

    refactor: reorder sections for optimal conversion funnel

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,10 +2,10 @@ import { HeroSection }      from '@/components/sections/HeroSection';
 import { TrustStrip }       from '@/components/sections/TrustStrip';
 import { StatsBar }         from '@/components/sections/StatsBar';
 import { ServicesSection }  from '@/components/sections/ServicesSection';
-import { ProcessSection }   from '@/components/sections/ProcessSection';
-import { AISection }        from '@/components/sections/AISection';
 import { PortfolioSection } from '@/components/sections/PortfolioSection';
+import { ProcessSection }   from '@/components/sections/ProcessSection';
 import { TestimonialsSection } from '@/components/sections/TestimonialsSection';
+import { AISection }        from '@/components/sections/AISection';
 import { PricingPreviewSection } from '@/components/sections/PricingPreviewSection';
 import { AboutSection }     from '@/components/sections/AboutSection';
 import { FooterCTA }        from '@/components/sections/FooterCTA';
@@ -17,10 +17,10 @@ export default function HomePage() {
       <TrustStrip />
       <StatsBar />
       <ServicesSection />
-      <ProcessSection />
-      <AISection />
       <PortfolioSection />
+      <ProcessSection />
       <TestimonialsSection />
+      <AISection />
       <PricingPreviewSection />
       <AboutSection />
       <FooterCTA />
